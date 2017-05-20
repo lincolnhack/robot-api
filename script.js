39b908cda3dca77f42c7be5edc34e508486f83de
David Wood <david.p.wood@gmail.com>
2017-05-20 22:20:46 +0100

    More NXT1

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function createRobot(comport, callback) {
     json: {
       name: "cword",
       type: "nxt1",
-      comport: "COM4"
+      comport
     }
   }, (err, res, body) => {
     if (err) {
@@ -40,17 +40,122 @@ function connectRobot(robotId, callback) {
       return
     }
 
-    callback()
+    callback(null, robotId)
+  })
+}
+
+function beepRobot(robotId, callback) {
+  console.log('beeping', robotId)
+  request.post({
+    url: `http://localhost:5000/robots/${robotId}/beep`,
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      callback(err)
+      return
+    }
+
+    if (res.statusCode !== 200) {
+      callback(new Error(`Unexpected status code ${res.statusCode}`))
+      return
+    }
+
+    callback(null, robotId)
+  })
+}
+
+function forwardRobot(robotId, callback) {
+  console.log('forwarding', robotId)
+  request.post({
+    url: `http://localhost:5000/robots/${robotId}/forward`,
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      callback(err)
+      return
+    }
+
+    if (res.statusCode !== 200) {
+      callback(new Error(`Unexpected status code ${res.statusCode}`))
+      return
+    }
+
+    callback(null, robotId)
+  })
+}
+
+function reverseRobot(robotId, callback) {
+  console.log('reversing', robotId)
+  request.post({
+    url: `http://localhost:5000/robots/${robotId}/reverse`,
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      callback(err)
+      return
+    }
+
+    if (res.statusCode !== 200) {
+      callback(new Error(`Unexpected status code ${res.statusCode}`))
+      return
+    }
+
+    callback(null, robotId)
+  })
+}
+
+function stopRobot(robotId, callback) {
+  console.log('stopping', robotId)
+  request.post({
+    url: `http://localhost:5000/robots/${robotId}/stop`,
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      callback(err)
+      return
+    }
+
+    if (res.statusCode !== 200) {
+      callback(new Error(`Unexpected status code ${res.statusCode}`))
+      return
+    }
+
+    callback(null, robotId)
+  })
+}
+
+function disconnectRobot(robotId, callback) {
+  console.log('disconnecting', robotId)
+  request.post({
+    url: `http://localhost:5000/robots/${robotId}/disconnect`,
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      callback(err)
+      return
+    }
+
+    if (res.statusCode !== 200) {
+      callback(new Error(`Unexpected status code ${res.statusCode}`))
+      return
+    }
+
+    callback(null, robotId)
   })
 }
 
 async.waterfall([
   async.apply(createRobot, 'COM4'),
-  async.apply(connectRobot)
+  async.apply(connectRobot),
+  async.apply(beepRobot),
+  async.apply(forwardRobot),
+  async.apply(reverseRobot),
+  async.apply(stopRobot),
+  async.apply(disconnectRobot)
 ], (err) => {
   if (err) {
     console.error(err)
     return
   }
-  console.log('did it')
+  console.log('we are done')
 })
