908a41b2f89ff4e7bcd3e577301c84370b56b896
David Wood <david.p.wood@gmail.com>
2017-05-21 10:46:52 +0100

    Scripty and keepalives

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const async = require('async')
 const request = require('request')
 
-function createRobot(comport, callback) {
+function createRobot(type, comport, callback) {
+  comport = comport || 'COM0'
   request.post({
     url: 'http://localhost:5000/robots',
     json: {
-      name: "cword",
-      type: "ev3",
+      name: `${type}:${comport}`,
+      type: type,
       comport
     }
   }, (err, res, body) => {
@@ -17,6 +18,7 @@ function createRobot(comport, callback) {
 
     if (res.statusCode !== 201) {
       callback(new Error(`Unexpected status code ${res.statusCode}`))
+      console.log(body)
       return
     }
 
@@ -164,15 +166,79 @@ function disconnectRobot(robotId, callback) {
   })
 }
 
-async.waterfall([
-  async.apply(createRobot, 'COM4'),
-  async.apply(connectRobot),
-  async.apply(beepRobot),
-  async.apply(forwardRobot),
-  async.apply(reverseRobot),
-  async.apply(leftRobot),
-  async.apply(rightRobot),
-  async.apply(disconnectRobot)
+let nxt1_id = null
+let nxt2_id = null
+let ev3_id = null
+let roomba_id = null
+
+async.series([
+  (cb) => async.parallel({
+    nxt1: async.apply(createRobot, 'nxt1', 'COM3'),
+    nxt2: async.apply(createRobot, 'nxt1', 'COM6'),
+    ev3: async.apply(createRobot, 'ev3', ''),
+    // roomba: async.apply(createRobot, 'roomba', ''),
+  }, (err, results) => {
+    if (err) {
+      cb(err)
+      return
+    }
+
+    console.log(results)
+
+    nxt1_id = results.nxt1
+    nxt2_id = results.nxt2
+    ev3_id = results.ev3
+    roomba_id = results.roomba
+
+    cb()
+  }),
+
+  // (cb) => {
+  //   console.log(`nxt1_id = ${nxt1_id}`)
+  //   console.log(`nxt2_id = ${nxt2_id}`)
+  //   console.log(`ev3_id = ${ev3_id}`)
+  //   console.log(`roomba_id = ${roomba_id}`)
+  //   cb()
+  // },
+
+  async.apply(async.parallel, [
+    (cb) => connectRobot(nxt1_id, cb),
+    (cb) => connectRobot(nxt2_id, cb)
+  ]),
+
+  // async.apply(async.parallel, [
+  //   (cb) => beepRobot(nxt1_id, cb),
+  //   (cb) => beepRobot(nxt2_id, cb)
+  // ]),
+
+  async.apply(async.parallel, [
+    (cb) => forwardRobot(nxt1_id, cb),
+    (cb) => forwardRobot(nxt2_id, cb),
+    (cb) => forwardRobot(ev3_id, cb),
+    // (cb) => forwardRobot(roomba_id, cb)
+  ]),
+
+  async.apply(async.parallel, [
+    (cb) => reverseRobot(nxt1_id, cb),
+    (cb) => reverseRobot(nxt2_id, cb),
+    (cb) => reverseRobot(ev3_id, cb),
+    // (cb) => reverseRobot(roomba_id, cb)
+  ]),
+
+  async.apply(async.parallel, [
+    (cb) => leftRobot(nxt1_id, cb),
+    (cb) => leftRobot(nxt2_id, cb),
+    (cb) => leftRobot(ev3_id, cb),
+    // (cb) => leftRobot(roomba_id, cb)
+  ]),
+
+  async.apply(async.parallel, [
+    (cb) => rightRobot(nxt1_id, cb),
+    (cb) => rightRobot(nxt2_id, cb),
+    (cb) => rightRobot(ev3_id, cb),
+    // (cb) => rightRobot(roomba_id, cb)
+  ]),
+
 ], (err) => {
   if (err) {
     console.error(err)
@@ -180,3 +246,4 @@ async.waterfall([
   }
   console.log('we are done')
 })
+
